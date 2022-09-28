f910c75d522fa03512d935d0749482f64b3e34e3
Pecze Dávid <peczedavid@gmail.com>
2022-09-28 15:50:34 +0200

    Update MessageListener.java

--- rabbitmq/rabbitmq-consumer/src/main/java/com/peczedavid/MessageListener.java
+++ rabbitmq/rabbitmq-consumer/src/main/java/com/peczedavid/MessageListener.java
@@ -2,15 +2,19 @@ package com.peczedavid;
 
 import com.peczedavid.config.RabbitMQConfing;
 import com.peczedavid.model.Message;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MessageListener {
 
+    Logger logger = LoggerFactory.getLogger(MessageListener.class);
+
     @RabbitListener(queues = RabbitMQConfing.MESSAGE_QUEUE_NAME)
     public void listener(Message message) {
-        System.out.println(message.getMessageDate() + " MESSAGE --- " + message.getMessage());
+        logger.info(message.getMessage());
     }
 
 }
